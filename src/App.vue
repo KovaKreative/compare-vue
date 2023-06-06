6c2ba162c4f19ccb78f6479b6005ab72f72d055e
KovaKreative <eric.kovalevskyy@gmail.com>
2023-06-06 15:22:19 -0400

    Style navbar

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,59 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <nav class="navbar has-shadow" role="navigation" aria-label="main navigation">
+      <div class="navbar-brand">
+        <a class="navbar-item">MyCompany</a>
+        <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+      <div class="navbar-menu navbar-end">
+        <router-link to="/" class="navbar-item r-item">Home</router-link>
+        <router-link to="/faq" class="navbar-item r-item">Features</router-link>
+        <router-link to="/faq" class="navbar-item r-item">About</router-link>
+        <router-link to="/faq" class="navbar-item r-item">FAQ</router-link>
+
+        <div class="navbar-item">
+          <p class="control">
+            <a class="button is-primary is-outlined">
+              <span class="icon">
+                <i class="fa fa-download"></i>
+              </span>
+              <span>Join Now</span>
+            </a>
+          </p>
+        </div>
+      </div>
+    </nav>
+    <router-view />
   </div>
 </template>
 
 <script>
 export default {
   name: 'App'
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="sass">
+@import '../node_modules/bulma/bulma.sass'
+@import 'mq'
+.navbar
+  background-color: #353540
+  a:hover
+    color: gray
+
+.navbar-brand a
+  color: #fed
+  font-weight: bold
+
+a.r-item
+  color: #C1C1C1
+  padding: 0.5rem 1.75rem
+  +mobile
+    color: gray
+    &:hover
+      background-color: #F1F1F5
 </style>
